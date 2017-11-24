9d559ba72a914f55f392298e780c332f2711e758
Marek Trunkat <marek@trunkat.eu>
2017-11-24 16:15:27 +0100

    Fixing timeout

--- src/modules/crawler.js
+++ src/modules/crawler.js
@@ -32,7 +32,7 @@ export default class Crawler extends EventEmitter {
         }
 
         if (crawlerConfig.pageLoadTimeout) {
-            this.gotoOptions.pageLoadTimeout = pageLoadTimeout;
+            this.gotoOptions.pageLoadTimeout = crawlerConfig.pageLoadTimeout;
         }
     }
 
