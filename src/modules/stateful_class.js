a4303da120cd0e090be23072c8d22154879c1e95
Marek Trunkat <marek@trunkat.eu>
2018-01-08 15:16:33 +0100

    Tmp - decreasing interval to persist state

--- src/modules/stateful_class.js
+++ src/modules/stateful_class.js
@@ -6,7 +6,7 @@
 import EventEmitter from 'events';
 import { logInfo } from './utils';
 
-const STATE_PERSIST_INTERVAL_MILLIS = 300 * 1000;
+const STATE_PERSIST_INTERVAL_MILLIS = 60 * 1000;
 
 export const EVENT_VALUE = 'value';
 
