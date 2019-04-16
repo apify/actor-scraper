8fecdc00a4bc443ab4fa02946aadd6ad57b01203
Ondra Urban <ondrejurban@hotmail.com>
2019-04-16 13:10:27 +0200

    Update Cheerio main

--- cheerio-scraper/src/actor.js
+++ cheerio-scraper/src/actor.js
@@ -3,22 +3,17 @@ const CrawlerSetup = require('./crawler_setup');
 
 const { utils: { log } } = Apify;
 
-log.logJson = false;
-
 Apify.main(async () => {
     log.debug('Reading INPUT.');
-    const input = await Apify.getValue('INPUT');
+    const input = await Apify.getInput();
     if (!input) throw new Error('INPUT cannot be empty!');
 
-    log.debug('Getting environment information.');
-    const env = Apify.getEnv();
-
     // Get crawler setup and startup options.
-    log.info('Configuring the Crawler.');
-    const setup = new CrawlerSetup(input, env);
+    log.info('Configuring Cheerio Scraper.');
+    const setup = new CrawlerSetup(input);
     const crawler = await setup.createCrawler();
 
-    log.info('Configuration completed. Starting the crawl.');
+    log.info('Configuration completed. Starting the scrape.');
     await crawler.run();
-    log.info('Crawler finished.');
+    log.info('Cheerio Scraper finished.');
 });
